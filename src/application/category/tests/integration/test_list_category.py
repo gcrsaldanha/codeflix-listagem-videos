058e9602469aaf60073b29851e635894b5f38280
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2024-06-24 22:20:35 -0300

    Move in memory repository to stub folder

--- src/application/category/tests/integration/test_list_category.py
+++ src/application/category/tests/integration/test_list_category.py
@@ -4,9 +4,7 @@ from src.application.category.list_category import ListCategory
 from src.application.category.tests.factories import CategoryFactory
 from src.application.listing import ListOutputMeta
 from src.domain.category.category import Category
-from src.infra.tests.in_memory_category_repository import (
-    InMemoryCategoryRepository,
-)
+from src.infra.elasticsearch.stub.in_memory_category_repository import InMemoryCategoryRepository
 
 
 class TestListCategory:
