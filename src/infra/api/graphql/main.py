d8a9d2d15a11dec10e0bfa4612ca9e957eaa0da3
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2024-07-09 12:13:07 -0300

    fix graphql run command

--- src/infra/api/graphql/main.py
+++ src/infra/api/graphql/main.py
@@ -4,4 +4,4 @@ from src.infra.api.graphql.schemas import schema
 
 graphql_app = GraphQLRouter(schema)
 
-# Run: strawberry server src.api.graphql.app.schema --port 8001
+# Run: strawberry server src.infra.api.graphql.main --port 8001
