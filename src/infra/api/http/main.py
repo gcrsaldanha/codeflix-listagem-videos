858d0a5c87758bab37d5a8041aebb3dd1a3ce246
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2024-07-10 09:46:21 -0300

    Control GraphQL via env var

--- src/infra/api/http/main.py
+++ src/infra/api/http/main.py
@@ -3,8 +3,8 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
+from src.config import ENABLE_GRAPHQL
 from src.infra.api.http.category_router import router as category_router
-from src.infra.api.graphql.main import graphql_app
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -18,5 +18,9 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-app.include_router(graphql_app, prefix="/graphql")
 app.include_router(category_router, prefix="/categories")
+
+if ENABLE_GRAPHQL:
+    from src.infra.api.graphql.main import graphql_app
+
+    app.include_router(graphql_app, prefix="/graphql")
