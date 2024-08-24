76e310681e02e401e8c88b13821262ebbca8ade5
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2024-08-24 14:56:22 -0300

    Fix warning elasticsearch legacy templates

--- src/infra/elasticsearch/client.py
+++ src/infra/elasticsearch/client.py
@@ -10,15 +10,12 @@ INDEXES = [
     CATEGORY_INDEX,
     GENRE_INDEX,
     GENRE_CATEGORY_INDEX,
-    # "cast_members",
-    # "videos",
 ]
-
 
 _es_instance = None
 
 
-def get_elasticsearch(host: str = ""):
+def get_elasticsearch(host: str = "") -> Elasticsearch:
     global _es_instance
 
     if _es_instance is None:
@@ -27,18 +24,17 @@ def get_elasticsearch(host: str = ""):
         for index in INDEXES:
             if not _es_instance.indices.exists(index=index):
                 _es_instance.indices.create(index=index)
+                print(f"Created index: {index}")
 
-        # Create a template for UUID fields (keyword)
+        # Create a index template for UUID fields (keyword)
+        if _es_instance.indices.exists_template(name="uuid_template"):
+            _es_instance.indices.delete_template(name="uuid_template")
+            print("Deleted existing uuid_template.")
+
         template_body = {
-            "index_patterns": ["*"],
-            "mappings": {
-                "properties": {
-                    "id": {
-                        "type": "keyword"
-                    }
-                }
-            }
+            "index_patterns": ["catalog-db.codeflix.*"],  # Specific to your indexes
+            "template": {"mappings": {"properties": {"id": {"type": "keyword"}}}},
         }
-        _es_instance.indices.put_template(name="uuid_template", body=template_body)
+        _es_instance.indices.put_index_template(name="uuid_template", body=template_body)
 
     return _es_instance
